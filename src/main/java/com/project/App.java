a367ab854fc75561265d9a3106f99608c82a1939
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-25 00:34:14 -0500

    add endpoint categories

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,6 +1,8 @@
 package com.project;
 
+import com.project.entity.Category;
 import com.project.entity.Supplier;
+import com.project.repository.CategoryRepository;
 import com.project.repository.SupplierRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,25 +13,36 @@ import java.util.List;
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
-	ApplicationContext context = SpringApplication.run(App.class, args);
-		SupplierRepository supplierRepo = context.getBean(SupplierRepository.class);
-		List<Supplier>suppliers = List.of(
-				new Supplier(null,"Supplier1","supplier1@gmail",true,"15478936"),
-				new Supplier(null,"Supplier2","supplier2@gmail",false,"1247896"),
-				new Supplier(null,"Supplier3","supplier3@gmail",false,"126985"),
-				new Supplier(null,"Supplier4","supplier4@gmail",false,"748596"),
-				new Supplier(null,"Supplier5","supplier5@gmail",false,"1478592")
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        SupplierRepository supplierRepo = context.getBean(SupplierRepository.class);
+        CategoryRepository categoryRepo = context.getBean(CategoryRepository.class);
+        List<Supplier> suppliers = List.of(
+                new Supplier(null, "Supplier1", "supplier1@gmail", true, "15478936"),
+                new Supplier(null, "Supplier2", "supplier2@gmail", false, "1247896"),
+                new Supplier(null, "Supplier3", "supplier3@gmail", false, "126985"),
+                new Supplier(null, "Supplier4", "supplier4@gmail", false, "748596"),
+                new Supplier(null, "Supplier5", "supplier5@gmail", false, "1478592")
 
 
-		);
+        );
+        supplierRepo.saveAll(suppliers);
+
+        List<Category> categories = List.of(
+                new Category(null, "Categoria1", "Buen estado", true),
+                new Category(null, "Categoria2", "Buen estado", false),
+                new Category(null, "Categoria3", "Buen estado", true),
+                new Category(null, "Categoria4", "Buen estado", false),
+                new Category(null, "Categoria5", "Buen estado", true),
+                new Category(null, "Categoria6", "Buen estado", false),
+                new Category(null, "Categoria7", "Buen estado", false),
+                new Category(null, "Categoria8", "Buen estado", true)
+
+        );
+
+        categoryRepo.saveAll(categories);
 
 
-		supplierRepo.saveAll(suppliers);
-
-
-
-
-	}
+    }
 
 }
---
+++ src/main/java/com/project/entity/Category.java
@@ -0,0 +1,28 @@
+package com.project.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Column(unique = true)
+    private String name;
+    @NotNull
+    private String description;
+    @NotNull
+    private Boolean isActive;
+
+}
