bfadd24797673060ef05db61b45d8ed334f17d71
JhonMohamed <162652096+JhonMohamed@users.noreply.github.com>
2024-10-04 16:41:14 -0500

    add files endpoint kardex

--- src/main/java/com/project/controller/KardexController.java
+++ src/main/java/com/project/controller/KardexController.java
@@ -2,10 +2,16 @@ package com.project.controller;
 
 import com.project.entity.Kardex;
 import com.project.service.KardexService;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import net.sf.jasperreports.engine.JRException;
+import org.springframework.http.ContentDisposition;
+import org.springframework.web.server.ResponseStatusException;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,36 +19,53 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class KardexController {
     @Autowired
-    KardexService service;
+    private KardexService service;
 
     @GetMapping("/kardex")
-    public List<Kardex> findAllKardex(){
+    public List<Kardex> findAllKardex() {
         return service.findAllKardex();
     }
 
     @GetMapping("/kardex/{id}")
-    public Kardex findById(@PathVariable Long id){
+    public Kardex findById(@PathVariable Long id) {
         Optional<Kardex> kardex = service.findOneById(id);
-        return kardex.orElseThrow(null);
+        return kardex.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Kardex not found"));
     }
 
     @PostMapping("/kardex")
-    public ResponseEntity<Kardex> createKardex(@RequestBody Kardex kardex){
+    public ResponseEntity<Kardex> createKardex(@RequestBody Kardex kardex) {
         Kardex createdKardex = service.create(kardex);
         return ResponseEntity.ok(createdKardex);
     }
 
     @PutMapping("/kardex/{id}")
-    public ResponseEntity<Kardex> updateKardex(@PathVariable Long id, @RequestBody Kardex kardex){
+    public ResponseEntity<Kardex> updateKardex(@PathVariable Long id, @RequestBody Kardex kardex) {
         Kardex updatedKardex = service.update(id, kardex);
         return ResponseEntity.ok(updatedKardex);
     }
 
     @DeleteMapping("/kardex/{id}")
-    public ResponseEntity<Kardex> deleteKardex(@PathVariable Long id){
+    public ResponseEntity<Kardex> deleteKardex(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/export-pdf")
+    public ResponseEntity<byte[]> exportPdf() throws JRException, FileNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("BocetoProyect", "petsReport.pdf");
+        return ResponseEntity.ok().headers(headers).body(service.exportPdf()); // Cambiado
+    }
 
+    @GetMapping("/export-xls")
+    public ResponseEntity<byte[]> exportXls() throws JRException, FileNotFoundException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8");
+        var contentDisposition = ContentDisposition.builder("attachment")
+                .filename("BocetoProyect" + ".xls").build();
+        headers.setContentDisposition(contentDisposition);
+        return ResponseEntity.ok().headers(headers).body(service.exportXls()); // Cambiado
+    }
 }
+
